3f75ddeb7d6e3d9cbd3a3f409e5a7972a9b74d6d
WebPal Cloud <webpal@palominosys.com>
2025-12-22 17:58:29 -0500

    fix: use QueryCursor from tree_sitter

--- aider/repomap.py
+++ aider/repomap.py
@@ -20,12 +20,12 @@ from tqdm import tqdm
 from aider.dump import dump
 from aider.special import filter_important_files
 from aider.waiting import Spinner
+from tree_sitter import QueryCursor
 
 # tree_sitter is throwing a FutureWarning
 warnings.simplefilter("ignore", category=FutureWarning)
 from grep_ast.tsl import USING_TSL_PACK, get_language, get_parser  # noqa: E402
 
-from tree_sitter import QueryCursor
 
 Tag = namedtuple("Tag", "rel_fname fname line name kind".split())
 
